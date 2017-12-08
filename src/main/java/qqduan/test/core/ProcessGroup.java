87abef98f8f6cc69067c4198c04efb3cb302415b
duanyijun <duanyijun@cloudwalk.cn>
2017-12-08 19:29:41 +0800

    4th

--- src/main/java/qqduan/test/core/ProcessGroup.java
+++ src/main/java/qqduan/test/core/ProcessGroup.java
@@ -3,10 +3,17 @@ package qqduan.test.core;
 import java.util.HashMap;
 import java.util.Map;
 
+import qqduan.test.interfac.GroupAfter;
+import qqduan.test.interfac.GroupBefore;
+
 public class ProcessGroup {
 
 	ProcessChain tmplate;
 	Map<String, ProcessChain> group;
+	boolean isSuccess;
+	
+	GroupBefore groupBefore;
+	GroupAfter groupAfter;
 
 	public ProcessGroup(ProcessChain tmplate) {
 		super();
